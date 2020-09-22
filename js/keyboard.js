09ba381f2262817fefa578e4e16b9accd12ede6c
José Vitor Sant'Ana <zevitorsant@gmail.com>
2020-09-22 06:36:32 -0300

    css do keyboard mais media

--- js/keyboard.js
+++ js/keyboard.js
@@ -1,44 +1,15 @@
 let audioContext = new (window.AudioContext || window.webkitAudioContext)();
 let masterGainNode = null;
+let oscList = null
 let volume = document.querySelector("#volume")
+let oitava = document.querySelector("#freq")
+let freq0 = parseInt(oitava.value)
+let freq1 = freq0 + 1
 let wavePicker = document.querySelector("select[name='waveform']");
 const notas = createNoteTable()
 
 setup()
 
-function createNoteTable(){
-    const notas = [
-        {
-            texto: "A",
-            freq: 261.63
-        },
-        {
-            texto: "B",
-            freq: 293.66
-        },
-        {
-            texto: "C",
-            freq: 329.63
-        },
-        {
-            texto: "D",
-            freq: 349.23
-        },
-        {
-            texto: "E",
-            freq: 391.99
-        },
-        {
-            texto: "F",
-            freq: 440.00
-        },
-        {
-            texto: "G",
-            freq: 493.88
-        }
-    ]
-    return notas
-}
 
 function setup(){
     masterGainNode = audioContext.createGain();
@@ -46,9 +17,17 @@ function setup(){
     masterGainNode.gain.value = volume.value;
 
     volume.addEventListener("change",changeVolume)
+    oitava.addEventListener("change",changeFrequency)
 
+    createKeyboard()
+}
+
+function createKeyboard(){
     for(let cont = 0; cont < Object.keys(notas).length; cont++){
-        createKey(notas[cont].texto,notas[cont].freq)
+        createKey(notas[cont].texto,notas[cont].freq, 0)
+    }
+    for(let cont = 0; cont < Object.keys(notas).length; cont++){
+        createKey(notas[cont].texto,notas[cont].freq, 1)
     }
 }
 
@@ -65,19 +44,39 @@ function playTone(freq) {
     return osc;
 }
 
-function createKey(nota, freq){
+function createKey(nota, freq, part){
     let keyboard = document.querySelector(".keyboard")
     let tecla = document.createElement("input")
     tecla.setAttribute("type","button")
-    tecla.value = nota
-    tecla.classList.add("tecla")
     keyboard.appendChild(tecla)
 
-    tecla.addEventListener("mousedown",function(event){
-        oscList = playTone(freq)
-    })
+    //tecla.value = nota
+
+    if(nota.length === 1) 
+        tecla.classList.add("tecla")
+    else
+        tecla.classList.add("newtecla")
+
+    //eventos de mouse pressionado
+
+    if(!part){
+        tecla.addEventListener("mousedown",function(event){
+            oscList = playTone(freq[freq0])
+        })
+    }
+    else{
+        tecla.addEventListener("mousedown",function(event){
+            oscList = playTone(freq[freq1])
+        })
+    }
     tecla.addEventListener("mouseup",noteReleased)
     tecla.addEventListener("mouseleave", noteReleased)
+
+    // eventos de toque mobile
+    tecla.addEventListener("touchstart",function(event){
+        oscList = playTone(freq[(oitava.value)])
+    })
+    tecla.addEventListener("touchend",noteReleased)
 }
 
 function noteReleased(event){
@@ -86,4 +85,9 @@ function noteReleased(event){
 
 function changeVolume(){
     masterGainNode.gain.value = volume.value
+}
+
+function changeFrequency(){
+    freq0 = parseInt(oitava.value)
+    freq1 = freq0 + 1
 }
